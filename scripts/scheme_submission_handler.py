e767b61792ca339b37dc6509d3dbbc4cc7925807
Reto Trappitsch <reto@galactic-forensics.space>
2024-04-29 14:14:05 +0200

    add scheme_addition workflow and python script

--- scripts/scheme_submission_handler.py
+++ scripts/scheme_submission_handler.py
@@ -1,24 +1,37 @@
 """This python file handles the submission of new schemes to the database."""
 
+import json
 from pathlib import Path
 import sys
 
 DB_PATH = Path(__file__).parents[1].joinpath("db")
 
 
-def add_to_db(json_str: str) -> None:
+def add_to_db(json_str: str) -> str:
     """Add a given json content to the database.
 
     :param json_str: JSON file content, as a string.
+
+    :return: Name of the created file.
     """
-    print(json_str)
+    json_str = json.loads(json_str)
+    element = json_str["rims_scheme"]["scheme"]["element"].lower()
 
-    # todo:
-    # 1: convert the json to an actual dictionary
-    # 2: get the element
-    # 3: determine the path and see what files are already present, then generate filename
-    # 4: save json file under new filename
+    # create the file name
+    it = 1
+    while True:
+        fname = DB_PATH.joinpath(f"{element}-{str(it).zfill(3)}.json")
+        if not fname.exists():
+            break
+        it += 1
 
+    with open(fname, "w") as f:
+        json.dump(json_str, f, indent=4)
+
+    return str(fname.name)
+
+
+# Main function called when the script is run with the json string as an argument
 
 if __name__ == "__main__":
     try:
@@ -28,4 +41,13 @@ if __name__ == "__main__":
             'No arguments provided. Please provide the content of the json file in "".'
         ) from e
 
-    add_to_db(arg_json)
+    retval = add_to_db(arg_json)
+    print(retval)
+
+# SOME MANUAL TESTS FOR THE FUNCTION ABOVE #
+
+
+def test_manual():
+    """Add a new element to the database."""
+    json_str = '{"rims_scheme": {"scheme": {"element": "U", "ion": 1}}}'
+    add_to_db(json_str)
